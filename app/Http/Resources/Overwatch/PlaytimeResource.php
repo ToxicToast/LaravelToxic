61056175e6b7837182b6e439e70275eadf0baa4f
Tom <info@toxictoast.de>
2018-03-26 02:00:41 +0200

    added roles to playtime

--- app/Http/Resources/Overwatch/PlaytimeResource.php
+++ app/Http/Resources/Overwatch/PlaytimeResource.php
@@ -16,8 +16,45 @@ class PlaytimeResource extends JsonResource
     {
         return [
             'id'            => $this->player_id,
-						'character'			=> $this->character_name,
-						'time'					=> $this->character_time
+            'character'		=> $this->character_name,
+            'time'		    => $this->character_time,
+            'role'          => $this->setCharacterRoles($this->character_name)
         ];
+    }
+
+    private function setCharacterRoles($character) {
+        $array = [];
+        //
+        $array['moira'] = 'Support';
+        $array['mercy'] = 'Support';
+        $array['zenyatta'] = 'Support';
+        $array['ana'] = 'Support';
+        $array['lucio'] = 'Support';
+        $array['symmetra'] = 'Support';
+        //
+        $array['dva'] = 'Tank';
+        $array['reinhardt'] = 'Tank';
+        $array['roadhog'] = 'Tank';
+        $array['winston'] = 'Tank';
+        $array['zarya'] = 'Tank';
+        $array['orisa'] = 'Tank';
+        //
+        $array['genji'] = 'Offense';
+        $array['soldier76'] = 'Offense';
+        $array['tracer'] = 'Offense';
+        $array['mccree'] = 'Offense';
+        $array['pharah'] = 'Offense';
+        $array['reaper'] = 'Offense';
+        $array['sombra'] = 'Offense';
+        $array['doomfist'] = 'Offense';
+        //
+        $array['junkrat'] = 'Defense';
+        $array['widowmaker'] = 'Defense';
+        $array['hanzo'] = 'Defense';
+        $array['mei'] = 'Defense';
+        $array['torbjorn'] = 'Defense';
+        $array['bastion'] = 'Defense';
+        //
+        return $array[$character];
     }
 }
